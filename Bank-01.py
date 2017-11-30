5637196607e1b78c2889543a7ca49a4d84b0945a
Bank Line/BOT <bank05628@gmail.com>
2017-11-30 19:40:02 +0700

    Update Bank-01.py

--- Bank-01.py
+++ Bank-01.py
@@ -545,28 +545,32 @@ def bot(op):
                     cl.sendText(msg.to,"􀜁􀇔􏿿Update Names👉 " + string + "👈")
 
 #-------------Fungsi Tag All Start---------------#
-            elif msg.text in ["แท็ค"]:
-              if msg.from_ in admin:
-                group = cl.getGroup(msg.to)
-                nama = [contact.mid for contact in group.members]
-                cb = ""
-                cb2 = ""
-                strt = int(0)
-                akh = int(0)
-                for md in nama:
-                    akh = akh + int(5)
-                    cb += """{"S":"""+json.dumps(str(strt))+""","E":"""+json.dumps(str(akh))+""","M":"""+json.dumps(md)+"},"""
-                    strt = strt + int(6)
-                    akh = akh + 1
-                    cb2 += "@nrik\n"
-                cb = (cb[:int(len(cb)-1)])
-                msg.contentType = 0
-                msg.text = cb2
-                msg.contentMetadata ={'MENTION':'{"MENTIONEES":['+cb+']}','EMTVER':'4'}
-                try:
-                    cl.sendMessage(msg)
-                except Exception as error:
-                    print error
+            elif msg.text in ["Cipok","แท็ค"]:
+                  group = cl.getGroup(msg.to)
+                  nama = [contact.mid for contact in group.members]
+
+                  cb = ""
+                  cb2 = ""
+                  strt = int(0)
+                  akh = int(0)
+                  for md in nama:
+                      akh = akh + int(6)
+
+                      cb += """{"S":"""+json.dumps(str(strt))+""","E":"""+json.dumps(str(akh))+""","M":"""+json.dumps(md)+"},"""
+
+                      strt = strt + int(7)
+                      akh = akh + 1
+                      cb2 += "@nrik \n"
+
+                  cb = (cb[:int(len(cb)-1)])
+                  msg.contentType = 0
+                  msg.text = cb2
+                  msg.contentMetadata ={'MENTION':'{"MENTIONEES":['+cb+']}','EMTVER':'4'}
+
+                  try:
+                      cl.sendMessage(msg)
+                  except Exception as error:
+                      print error
 #-------------Fungsi Tag All Finish---------------#
 
 #---------------------------------------------------------
